f2e832ba1f9b0b1cb7cfd527d72157a7112464f5
Feyduş Kıymet Kocahan <162510667+feydus@users.noreply.github.com>
2025-03-22 02:44:40 +0300

    Add files via upload

--- main.swift
+++ main.swift
@@ -29,5 +29,34 @@ print(sayilar)
 sayilar.removeAll() //dizinin tüm elemanlarını kaldırma
 print(sayilar)
 
+//sözlük yapısı
+
+var emptyDictionary: [String: Int] = [:] //boş sözlük bildirme
+var emptyDic = [String: Int] () //boş sözlük bildirme
+var studentScores: [String: Int] = ["alice" : 85, "bob" : 92,"charlie" : 78]
+var studentSc = ["ali" : 85 , "bobi" : 44 , "crls" : 55] //swift türleri algılar
+
+//köşeli parantez ve bir anahtar kullanarak değerleri alabilirsiniz
+print(studentScores["alice"]) //çıkış 85
+print(studentScores["bob"]) //çıkış 92
+print(studentScores["charlie"]) //çıkış 78
+
+//Since the key might not exist, the result is Optional.
+//To safely unwrap it:
+if let score = studentScores["alice"] {
+    print("alice's score is  \(score)")
+          }else{
+        print("alice's score not found")
+    }
+
+studentScores["david"] = 88
+print(studentScores)
+studentScores.removeValue(forKey: "david")
+print(studentScores)
+studentScores["alice"] = nil
+print(studentScores)
+studentScores.removeAll()
+print(studentScores)
+
 
 
